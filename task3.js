ca77392aae63bc743057ad6948d3cf0553eeb843
xel26 <zhxo111nvshq@gmail.com>
2023-11-01 00:22:19 +0700

    memperbaiki task-3

--- task3.js
+++ task3.js
@@ -1,30 +1,35 @@
-const getName = (callback) => {                                             // membuat function getName yang menerima parameter callback
-    fetch("https://jsonplaceholder.typicode.com/users")                     // melakukan fetch yg mereturn promise
-    .then(response => {                                                     // menghandle hasil eksekusi fetch yang sukses atau resolve
-        callback(null, response)                                                  // memanggil callback dan mengirim hasil fetch di parameter callback
-    })
-    .catch (error => {                                                      // menghandle hasil eksekusi fetch yang gagal atau reject
-        callback(`${error.message} data not found`, {})                     // menampilkan pesan error
-    })
-}
+const data = fetch("https://jsonplaceholder.typicode.com/users")
 
 
 
 
 
-const display = (error, response) => {                                             // membuat function display yang menerima dua parameter 
-    if(error === null){                                                            // jika error berisi nilai null
-        response.json()                                                            // parsing data json yang mereturn promise. mengubah tipe data json menjadi array of object
-        .then(result => {                                                          // menghandle hasil sukses atau resolve dari promise yang di return oleh metode json()
-            result.forEach(object => {                                             // melooping tiap object dalam array
-                console.log(object.name)                                           // mengambil nama dalam object dan menampilkannya
-            });
+const display = (response) => {
+    if(response.status === 200){
+        response.json().then(result => {
+            result.forEach(object => console.log(object.name))
         })
-    }else{                                                                        // jika error tidak berisi nilai null atau berisi error
-        console.log(error, response)                                              // menampilkan pesan error
+    }else {
+        console.log("data not found")
     }
 }
 
 
 
-getName(display)                                                                   // memanggil fungsi getName dengan mengirim fungsi display sebagai parameter
+
+// then catch :
+data.then(display).catch (error => console.log(error.message))
+
+
+
+
+// async await :
+const getName = async (callback) => {
+    try {
+        let response = await data
+        callback(response)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+getName(display)
